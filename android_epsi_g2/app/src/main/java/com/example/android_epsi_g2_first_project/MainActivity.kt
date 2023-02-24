919492c51fa53b7b040cc3c63a4f1fcbb0805d02
sangmin <tkdals4723@gmail.com>
2023-02-24 10:24:37 +0100

    Header Title Done

--- android_epsi_g2/app/src/main/java/com/example/android_epsi_g2_first_project/MainActivity.kt
+++ android_epsi_g2/app/src/main/java/com/example/android_epsi_g2_first_project/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.android_epsi_g2_first_project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setHeaderTitle(getString(R.string.main_title))
     }
 }
