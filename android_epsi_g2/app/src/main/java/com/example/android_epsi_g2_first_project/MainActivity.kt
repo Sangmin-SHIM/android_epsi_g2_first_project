9f8af40b08bc9b7eb440514b4735ade92a2fa16c
sangmin <tkdals4723@gmail.com>
2023-03-09 21:06:26 +0100

    3 button added in Detail Activity (INFO)

--- android_epsi_g2/app/src/main/java/com/example/android_epsi_g2_first_project/MainActivity.kt
+++ android_epsi_g2/app/src/main/java/com/example/android_epsi_g2_first_project/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.android_epsi_g2_first_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setHeaderTitle(getString(R.string.main_title))
+
+        // Info Button
+        var buttonInfo=findViewById<Button>(R.id.buttonInfo)
+        buttonInfo.setOnClickListener(View.OnClickListener() {
+            val newIntent = Intent(application, DetailsActivity::class.java)
+            startActivity(newIntent)
+            })
     }
 }
