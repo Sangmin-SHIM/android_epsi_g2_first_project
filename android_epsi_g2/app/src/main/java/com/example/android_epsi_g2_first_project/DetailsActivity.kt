c257c326f661d64d6f2545d93d0b13d72b34ac09
sangmin <tkdals4723@gmail.com>
2023-03-10 19:24:56 +0100

    INFO Etudiant done !

--- android_epsi_g2/app/src/main/java/com/example/android_epsi_g2_first_project/DetailsActivity.kt
+++ android_epsi_g2/app/src/main/java/com/example/android_epsi_g2_first_project/DetailsActivity.kt
@@ -10,6 +10,7 @@ import com.squareup.picasso.Picasso
 
 class DetailsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
         setHeaderTitle(getString(R.string.info_title))
@@ -18,22 +19,33 @@ class DetailsActivity : BaseActivity() {
         // Student1 Button
         var buttonStudent1=findViewById<Button>(R.id.buttonStudent1)
         buttonStudent1.setOnClickListener(View.OnClickListener() {
-            val newIntent1 = Intent(application, StudentActivity::class.java)
-            startActivity(newIntent1)
+            val studentName1=getString(R.string.student1_name)
+            val studentEmail1=getString(R.string.student1_mail)
+            showStudentActivity(studentName1, studentEmail1, "man");
         })
 
         // Student2 Button
         var buttonStudent2=findViewById<Button>(R.id.buttonStudent2)
         buttonStudent2.setOnClickListener(View.OnClickListener() {
-            val newIntent2 = Intent(application, StudentActivity::class.java)
-            startActivity(newIntent2)
+            val studentName2=getString(R.string.student2_name)
+            val studentEmail2=getString(R.string.student2_mail)
+            showStudentActivity(studentName2, studentEmail2, "man");
         })
 
         // Student3 Button
         var buttonStudent3=findViewById<Button>(R.id.buttonStudent3)
         buttonStudent3.setOnClickListener(View.OnClickListener() {
-            val newIntent3 = Intent(application, StudentActivity::class.java)
-            startActivity(newIntent3)
+            val studentName3=getString(R.string.student3_name)
+            val studentEmail3=getString(R.string.student3_mail)
+            showStudentActivity(studentName3, studentEmail3, "woman");
         })
+    }
+
+    private fun showStudentActivity(studentName:String,studentEmail:String, sex:String) {
+        val newIntent = Intent(application, StudentActivity::class.java)
+        newIntent.putExtra("studentName", studentName)
+        newIntent.putExtra("studentEmail", studentEmail)
+        newIntent.putExtra("sex", sex)
+        startActivity(newIntent)
     }
 }
--- android_epsi_g2/app/src/main/java/com/example/android_epsi_g2_first_project/StudentActivity.kt
+++ android_epsi_g2/app/src/main/java/com/example/android_epsi_g2_first_project/StudentActivity.kt
@@ -1,11 +1,30 @@
 package com.example.android_epsi_g2_first_project
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
 
 class StudentActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
+        val studentName=intent.extras!!.getString("studentName")
+        val studentEmail=intent.extras!!.getString("studentEmail")
+        val sex=intent.extras!!.getString("sex")
+        val image=findViewById<ImageView>(R.id.imageViewAvatar)
+
+        if (sex.equals("man")){
+            image.setBackgroundResource(R.drawable.man);
+        } else {
+            image.setBackgroundResource(R.drawable.woman);
+        }
+        val textStudentName = findViewById<TextView>(R.id.textStudentName)
+        textStudentName.setText(studentName);
+
+        val textStudentEmail = findViewById<TextView>(R.id.textStudentEmail)
+        textStudentEmail.setText(studentEmail);
 
         setHeaderTitle(getString(R.string.info_student))
         showBack()
